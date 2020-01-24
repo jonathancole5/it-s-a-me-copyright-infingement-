b0dfda2ba5a358efc7820a36116a16cb63cfa1a2
jonathancole5 <37849318+jonathancole5@users.noreply.github.com>
2020-01-24 15:17:52 -0800

    Add files via upload

---
+++ it's a me, Copyright infringement!(Baseline)4.rb
@@ -0,0 +1,479 @@
+use_synth :piano
+use_bpm 175
+
+sample "C:/Users/jonathan_cole/Desktop/intro.wav"
+sleep 11
+
+live_loop :bass do
+  #MF 1
+  play :A2
+  sleep 2
+  play :A2
+  sleep 2
+  
+  #MF2
+  play :A2
+  sleep 0.5
+  play :A2
+  sleep 0.25
+  play :A3
+  sleep 0.25
+  play :A2
+  sleep 0.5
+  play :B2
+  sleep 0.5
+  play :B2
+  sleep 0.5
+  play :Bs2
+  sleep 0.5
+  play :Cs3
+  sleep 0.5
+  
+  #MF3
+  play :D3
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  play :A2
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  
+  #MF 4
+  play :D3
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  play :A2
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  
+  #MF5
+  play :D3
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  play :A2
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  
+  #MF6
+  play :D3
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  play :A2
+  sleep 0.5
+  play :A
+  play :D
+  play :Fs
+  sleep 0.5
+  
+  #MF7
+  play :G3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  
+  #MF8
+  play :G3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  play :D3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  
+  play :Gs3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Gs4
+  sleep 0.5
+  
+  play :D3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Gs4
+  sleep 0.5
+  
+  play :Gs3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Gs4
+  sleep 0.5
+  
+  play :D3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Gs4
+  sleep 0.5
+  
+  play :Fs3
+  sleep 0.5
+  play :A3
+  play :Cs4
+  play :E4
+  sleep 0.5
+  
+  play :Cs3
+  sleep 0.5
+  play :A3
+  play :Cs4
+  play :E4
+  sleep 0.5
+  
+  play :Fs3
+  sleep 0.5
+  play :A3
+  play :Cs4
+  play :E4
+  sleep 0.5
+  
+  play :Cs3
+  sleep 0.5
+  play :A3
+  play :Cs4
+  play :E4
+  sleep 0.5
+  
+  play :F3
+  sleep 0.5
+  play :A3
+  play :C4
+  play :Eb4
+  sleep 0.5
+  
+  play :C3
+  sleep 0.5
+  play :A3
+  play :C4
+  play :Eb4
+  sleep 0.5
+  
+  play :F3
+  sleep 0.5
+  play :A3
+  play :C4
+  play :Eb4
+  sleep 0.5
+  
+  play :C3
+  sleep 0.5
+  play :A3
+  play :C4
+  play :Eb4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :A3
+  sleep 0.5
+  play :Cs4
+  play :E4
+  play :A4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :Cs4
+  play :E4
+  play :A4
+  sleep 0.5
+  
+  play :A3
+  sleep 0.5
+  play :Cs4
+  play :E4
+  play :A4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :Cs4
+  play :E4
+  play :A4
+  sleep 0.5
+  
+  play :D3
+  sleep 0.5
+  play :A3
+  play :D4
+  play :Fs
+  sleep 0.5
+  
+  play :A2
+  sleep 0.5
+  play :A3
+  play :D4
+  play :Fs
+  sleep 0.5
+  
+  play :D3
+  sleep 0.5
+  play :A3
+  play :D4
+  play :Fs
+  sleep 0.5
+  
+  play :A2
+  sleep 0.5
+  play :A3
+  play :D4
+  play :Fs
+  sleep 0.5
+  
+  play :Ds3
+  sleep 0.5
+  play :A3
+  play :Ds4
+  play :Fs4
+  sleep 0.5
+  
+  play :A2
+  sleep 0.5
+  play :A3
+  play :Ds4
+  play :Fs4
+  sleep 0.5
+  
+  play :Ds3
+  sleep 0.5
+  play :A3
+  play :Ds4
+  play :Fs4
+  sleep 0.5
+  
+  play :A2
+  sleep 0.5
+  play :A3
+  play :Ds4
+  play :Fs4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :B3
+  play :D4
+  play :G4
+  sleep 0.5
+  
+  play :F3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Ab4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Ab4
+  sleep 0.5
+  
+  play :F3
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Ab4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :B3
+  play :D4
+  play :Ab4
+  sleep 0.5
+  
+  play :Fs3
+  sleep 0.5
+  play :A3
+  play :Db4
+  play :E4
+  sleep 0.5
+  
+  play :Fs3
+  sleep 0.5
+  play :A3
+  play :Cs4
+  play :E4
+  sleep 0.5
+  
+  play :F3
+  sleep 0.5
+  play :A3
+  play :C4
+  play :Eb4
+  sleep 0.5
+  
+  play :F3
+  sleep 0.5
+  play :A3
+  play :C4
+  play :D4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :E3
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :Eb3
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :Eb3
+  sleep 0.5
+  play :B3
+  play :E4
+  play :G4
+  sleep 0.5
+  
+  play :D3
+  sleep 0.5
+  play :Cs4
+  play :D4
+  play :FS4
+  sleep 0.5
+  
+  play :D3
+  sleep 0.5
+  play :Cs4
+  play :D4
+  play :FS4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :Cs4
+  play :D4
+  play :FS4
+  sleep 0.5
+  
+  play :B2
+  sleep 0.5
+  play :Cs4
+  play :D4
+  play :FS4
+  sleep 0.5
+  
+  play :Bb2
+  sleep 0.5
+  play :A3
+  play :D4
+  play :F4
+  sleep 0.5
+  
+  play :Bb2
+  sleep 0.5
+  play :A3
+  play :D4
+  play :F4
+  sleep 0.5
+  
+  play :Eb3
+  sleep 0.5
+  play :Bb3
+  play :D4
+  play :G4
+  sleep 0.5
+  
+  play :Eb3
+  sleep 0.5
+  play :Bb3
+  play :D4
+  play :G4
+  sleep 0.5
+end
